74202dbf6345bb7201424a75ddfa6802d9e9379a
Bryan J. Thomas <132914063+bryanthxo@users.noreply.github.com>
2023-05-15 22:28:43 -0400

    dev

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+
+    const [ categories, setCategories ] = useState(['Rick and Morty']);
+
+    const onAddCategory = ( newCategory) => {
+
+        if( categories.includes(newCategory)) return;
+
+        // categories.push(newCategory);
+         setCategories( [ newCategory, ...categories ] );
+        // setCategories( cat => [...cat, 'Valorant']);
+    }
+
+    return (
+        <>
+        {/* titulo */}
+            <h1>GifExpertApp</h1>
+
+            <AddCategory 
+                onNewCategory={ (value) => onAddCategory(value) }
+                currentCategories={categories}
+            />
+
+            {
+            categories.map((category) => (
+                    <GifGrid 
+                        key={category} 
+                        category={category} 
+                    />
+                ))
+            }
+               
+
+
+        </>
+    )
+}
